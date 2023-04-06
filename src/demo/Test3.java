098145d10e4fdebe4cf860248a4113bc62d8048d
GYTumlisan <gymoa2011@gmail.com>
2023-04-05 19:30:59 -0700

    Initial limit

---
+++ src/demo/Test3.java
@@ -0,0 +1,11 @@
+package demo;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		System.out.println("test 3...");
+		
+		
+		System.out.println("Remote Repository....");
+	}
+}
